9b0df6551b1b01392569cc5e8e23c051268e79e9
Vnu <dearvnu@yahoo.in>
2012-11-17 20:31:10 -0800

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+	describe "Home page" do
+		it "should have the h1 'mBlog'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1',:text => 'mBlog')
+
+			page.should have_selector('title', :text => "mBlog using Ruby on Rails | Home")
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/home'
+
+			page.should have_selector('title', :text => "mBlog using Ruby on Rails | Home")
+		end
+	end
+
+	describe "Help page" do
+		it "should have the h1 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+
+			page.should have_selector('title', :text => "mBlog using Ruby on Rails | Help")
+		end
+	end
+
+	describe "About page" do
+		it "should have the h1 'About Us'" do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+
+			page.should have_selector('title', :text => "mBlog using Ruby on Rails | About Us")
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+
+			page.should have_selector('title', :text => "mBlog using Ruby on Rails | Contact")
+		end
+	end
+
+end

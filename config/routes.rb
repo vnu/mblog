38c0ba4222976a6248e5e9a7350bf6db0d2d012c
Vnu <dearvnu@yahoo.in>
2012-11-21 14:50:31 -0800

    Finish Sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Mblog::Application.routes.draw do
 
+  resources :users
+
   root to: 'static_pages#home'  
 
   match '/signup', to:'users#new'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 		"Xpres Infos"
+		email 		"xpresinfos@example.com"
+		password 	"foobar"
+		password_confirmation "foobar"
+	end
+end
